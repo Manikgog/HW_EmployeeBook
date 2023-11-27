2e44c9cf9fe0c87155975a0a810c66b9d7c4a74a
Manikgog <m4xin4t4@gmail.com>
2023-11-27 11:46:19 +0500

    Initial commit

---
+++ src/main/java/pro/sky/hwemployeebooktomap/HwemployeebooktomapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.hwemployeebooktomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwemployeebooktomapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwemployeebooktomapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/hwemployeebooktomap/HwemployeebooktomapApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hwemployeebooktomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwemployeebooktomapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
